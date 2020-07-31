926efd7810dd24fb7dd464ddeb3bb7fbb9a70a4e
FRsparrow <1528525449@qq.com>
2020-07-31 23:28:21 +0800

    graph

--- graph.cpp
+++ graph.cpp
@@ -5,35 +5,156 @@ using namespace std;
 
 int main()
 {
-    const int node_number = 5;
-    Graph* graph = new Graph(node_number);
-    graph->add_edge(0, 1, 1);
-    graph->add_edge(0, 2, 3);
-    graph->add_edge(0, 3, 2);
-    graph->add_edge(0, 4, 8);
-    graph->add_edge(1, 4, 4);
-    graph->add_edge(2, 3, 7);
-    graph->add_edge(2, 4, 5);
-    graph->add_edge(3, 4, 1);
+    const int node_number = 8;
+    Graph* graph = new Graph(node_number, false);
+    /*
+    (2)——————5————————
+     |               |
+     3  ————8——————  |
+     | /            \|
+    (0)——1——(1)——4——(4)
+      \             /
+       2   1————————
+        \ /
+        (3)
+    */
+    // 无向图
+    // graph->add_edge(0, 1, 1);
+    // graph->add_edge(0, 2, 3);
+    // graph->add_edge(0, 3, 2);
+    // graph->add_edge(0, 4, 8);
+    // graph->add_edge(1, 4, 4);
+    // graph->add_edge(2, 3, 7);
+    // graph->add_edge(2, 4, 5);
+    // graph->add_edge(3, 4, 1);
 
-    int d[node_number], path[node_number];
-    for (int i = 0; i < node_number; i++)
+    // 有向图
+    // graph->add_edge(0, 1, 6);
+    // graph->add_edge(0, 2, 5);
+    // graph->add_edge(0, 3, 5);
+    // graph->add_edge(1, 4, -1);
+    // graph->add_edge(2, 1, -2);
+    // graph->add_edge(2, 4, 1);
+    // graph->add_edge(3, 2, -2);
+    // graph->add_edge(3, 5, -1);
+    // graph->add_edge(4, 6, 3);
+    // graph->add_edge(5, 6, 3);
+
+    // 拓扑排序图1
+    // graph->add_edge(0, 1);
+    // graph->add_edge(0, 3);
+    // graph->add_edge(1, 5);
+    // graph->add_edge(2, 1);
+    // graph->add_edge(2, 5);
+    // graph->add_edge(4, 0);
+    // graph->add_edge(4, 1);
+    // graph->add_edge(4, 5);
+
+    // 拓扑排序图2
+    // graph->add_edge(0, 2);
+    // graph->add_edge(0, 7);
+    // graph->add_edge(1, 2);
+    // graph->add_edge(1, 3);
+    // graph->add_edge(1, 4);
+    // graph->add_edge(2, 3);
+    // graph->add_edge(3, 5);
+    // graph->add_edge(3, 6);
+    // graph->add_edge(4, 5);
+    // graph->add_edge(7, 8);
+    // graph->add_edge(8, 6);
+
+    // 关键路径图
+    graph->add_edge(0, 1, 8);
+    graph->add_edge(0, 2, 12);
+    graph->add_edge(1, 3, 14);
+    graph->add_edge(2, 3, 10);
+    graph->add_edge(2, 5, 28);
+    graph->add_edge(3, 4, 6);
+    graph->add_edge(3, 5, 8);
+    graph->add_edge(4, 6, 18);
+    graph->add_edge(5, 6, 6);
+    graph->add_edge(6, 7, 12);
+
+    // int d[node_number], path[node_number];
+    // for (int i = 0; i < node_number; i++)
+    // {
+    //     graph->bellman_ford(i, d, path);
+    //     for (int j = 0; j < node_number; j++)
+    //     {
+    //         // if (d[j] >= INF)
+    //         // {
+    //         //     cout << i << " can't get to " << j << endl;
+    //         //     continue;
+    //         // }
+    //         // cout << i << " to " << j << ": " << d[j] << ",path:";
+    //         cout << d[j] << "(";
+    //         int n = j;
+    //         while (d[j] < INF && n != i)
+    //         {
+    //             cout << n << "<-";
+    //             n = path[n];
+    //         }
+    //         cout << i << ")\t";
+    //         // cout << d[j] << "\t";
+    //     }
+    //     cout << endl;
+    // }
+    // cout << endl;
+    
+    // // 使用floyd算法
+    // int **a = new int*[node_number], **path2 = new int*[node_number];
+    // for (int i = 0; i < node_number; i++)
+    // {
+    //     a[i] = new int[node_number];
+    //     path2[i] = new int[node_number];
+    // }
+    // graph->floyd(a, path2);
+    // for (int i = 0; i < node_number; i++)
+    // {
+    //     for (int j = 0; j < node_number; j++)
+    //     {
+    //         cout << a[i][j] << "(";
+    //         int n = j;
+    //         while (a[i][j] < INF && n != i)
+    //         {
+    //             cout << n << "<-";
+    //             n = path2[i][n];
+    //         }
+    //         cout << i << ")\t";
+    //     }
+    //     cout << endl;
+    // }
+    
+    // 拓扑排序
+    // int seq[node_number];
+    // bool exist_circle = !graph->topological_sort(seq);
+    // if (exist_circle)
+    // {
+    //     cout << "it's an undirected graph or there exists a circle in the directed graph";
+    // }
+    // else
+    // {
+    //     cout << seq[0];
+    //     for (int i = 1; i < node_number; i++)
+    //     {
+    //         cout << "->" << seq[i];
+    //     }
+    // }
+    // cout << endl;
+    
+    // 关键路径
+    int cp[20];
+    for (int i = 0; i < 20; i++)
     {
-        graph->dijkstra(i, d, path);
-        for (int j = 0; j < node_number; j++)
-        {
-            cout << i << "到" << j << "路径长度为" << d[j] << "，反向路径为:";
-            int n = j;
-            while (n != i)
-            {
-                cout << n << " ";
-                n = path[n];
-            }
-            cout << i << endl;
-        }
-        cout << endl;
+        cp[i] = -1;
+    }
+    graph->critical_path(cp);
+    for (int i = 0; cp[i] != -1; i++)
+    {
+        cout << cp[i] << " ";
     }
     
+
     system("pause");
     return 0;
 }
--- myheaders/graph.h
+++ myheaders/graph.h
@@ -1,6 +1,7 @@
 #include "heap.h"
 
-#define MAX_NODE_NUMBER MAX_HEAP_SIZE
+#define MAX_NODE_NUMBER MAX_HEAP_SIZE   // 最大结点数
+#define INF 100    // 无穷权值
 
 // 堆优化dijkstra的堆元素类型
 struct heap_node
@@ -22,6 +23,7 @@ struct heap_node
     }
 };
 
+// 边
 class edge
 {
 public:
@@ -37,6 +39,7 @@ edge::edge(int n, int c): node(n), cost(c), next(nullptr){}
 
 edge::~edge(){ delete next;}
 
+// 结点
 class node
 {
 public:
@@ -66,20 +69,29 @@ void node::add_neighbour(int t, int cost)
     }
 }
 
+// 图
 class Graph
 {
 private:
+    int node_number;
+    node* sources;  // 正邻接表
+    node* targets;  // 逆邻接表,若为无向图则等于sources
+
+    bool undirected;    // true为无向图，false为有向图
     void set_distance(int s, int d[], Heap<heap_node>* heap);
+    void get_indegree(int count[], int& top);
 
 public:
-    int node_number;
-    node* nodes;
 
-    Graph(int n);
+    Graph(int n, bool undirected);
     ~Graph();
 
     void add_edge(int s, int t, int cost);
     void dijkstra(int s, int d[], int path[]);
+    void bellman_ford(int s, int d[], int path[]);
+    void floyd(int** a, int** path);
+    void critical_path(int* cp);
+    bool topological_sort(int seq[]);   // 有环或为无向图返回false，否则返回true
     int get_cost(int s, int t);
 };
 
@@ -87,12 +99,12 @@ void Graph::set_distance (int s, int d[], Heap<heap_node>* heap)
 {
     for (int i = 0; i < node_number; i++)
     {
-        d[i] = -1;
+        d[i] = INF;
     }
     d[s] = 0;
     heap->push(heap_node(s, 0));
     
-    // edge* p = nodes[s].neighbour;
+    // edge* p = sources[s].neighbour;
     // while (p)
     // {
     //     heap->push(heap_node(p->node, p->cost));
@@ -102,7 +114,26 @@ void Graph::set_distance (int s, int d[], Heap<heap_node>* heap)
     
 }
 
-Graph::Graph(int n)
+void Graph::get_indegree(int count[], int& top)
+{
+    for (int i = 0; i < node_number; i++)
+    {
+        count[i] = 0;
+        edge* p = targets[i].neighbour;
+        while (p)
+        {
+            ++count[i];
+            p = p->next;
+        }
+        if (!count[i])  // 入度为0，进栈
+        {
+            count[i] = top;
+            top = i;
+        }
+    }
+}
+
+Graph::Graph(int n, bool type=true)
 {
     if (n > MAX_NODE_NUMBER)
     {
@@ -110,18 +141,44 @@ Graph::Graph(int n)
     }
     else
     {
+        undirected = type;
         node_number = n;
-        nodes = new node[n];
+        sources = new node[n];
+
+        if (!type)   // 有向图建立逆邻接表
+        {
+            targets = new node[n];
+        }
+        else
+        {
+            targets = sources;
+        }
     }
      
 }
 
-Graph::~Graph(){ delete[] nodes; }
-
-void Graph::add_edge(int s, int t, int cost)
+Graph::~Graph()
 {
-    nodes[s].add_neighbour(t, cost);
-    nodes[t].add_neighbour(s, cost);
+    delete[] sources;
+    if (!undirected)
+    {
+        delete[] targets;
+    }
+}
+
+void Graph::add_edge(int s, int t, int cost=1)
+{
+    sources[s].add_neighbour(t, cost);
+    // 无向图则每条边有两个方向
+    if (undirected)
+    {
+        sources[t].add_neighbour(s, cost);
+    }
+    else
+    {
+        targets[t].add_neighbour(s, cost);
+    }
+    
 }
 
 int Graph::get_cost(int s, int t)
@@ -131,7 +188,7 @@ int Graph::get_cost(int s, int t)
         return 0;
     }
     
-    edge* p = nodes[s].neighbour;
+    edge* p = sources[s].neighbour;
     while (p)
     {
         if (p->node == t)
@@ -140,7 +197,7 @@ int Graph::get_cost(int s, int t)
         }
         p = p->next;
     }
-    return -1;
+    return INF;
 }
 
 void Graph::dijkstra(int s, int d[], int path[])
@@ -175,7 +232,7 @@ void Graph::dijkstra(int s, int d[], int path[])
             if (!computed[i])
             {
                 int c = get_cost(hn.node, i);
-                if(c != -1 && (d[i] == -1 || c + hn.cost < d[i]))
+                if(c != INF && (d[i] == INF || c + hn.cost < d[i]))
                 {
                     d[i] = c + hn.cost;
                     path[i] = hn.node;
@@ -184,4 +241,200 @@ void Graph::dijkstra(int s, int d[], int path[])
             }
         }
     }
+
+    delete heap;
+}
+
+void Graph::bellman_ford(int s, int d[], int path[])
+{
+    for (int i = 0; i < node_number; i++)
+    {
+        d[i] = INF;
+        path[i] = -1;
+    }
+    d[s] = 0;
+    edge* p = sources[s].neighbour;
+    // 源点s的所有邻居初始化的distance值初始化
+    while (p)
+    {
+        d[p->node] = p->cost;
+        path[p->node] = s;
+        p = p->next;
+    }
+    
+    for (int i = 1; i < node_number-1; i++)
+    {
+        for (int j = 0; j < node_number; j++)
+        {
+            // if (s != j)
+            // {
+            //     for (int k = 0; k < node_number; k++)
+            //     {
+            //         int c = get_cost(k, j);
+            //         if (c < INF && d[k] < INF && d[j] > d[k] + c)
+            //         {
+            //             d[j] = d[k] + c;
+            //             path[j] = k;
+            //         }
+            //     }
+            // }
+
+            edge* p = targets[j].neighbour;
+            // 遍历指向j的每条边
+            while (p)
+            {
+                if (d[p->node] < INF && d[j] > d[p->node] + p->cost)
+                {
+                    d[j] = d[p->node] + p->cost;
+                    path[j] = p->node;
+                }
+                p = p->next;
+            }
+            
+        }
+    }
+}
+
+void Graph::floyd(int** a, int** path)
+{
+    for (int i = 0; i < node_number; i++)
+    {
+        for (int j = 0; j < node_number; j++)
+        {
+            a[i][j] = INF;
+            path[i][j] = -1;
+        }
+        a[i][i] = 0;
+        path[i][i] = i;
+        // 初始化邻接矩阵
+        edge* p = sources[i].neighbour;
+        while (p)
+        {
+            path[i][p->node] = i;
+            a[i][p->node] = p->cost;
+            p = p->next;
+        }
+    }
+    
+    // i到j的路径中间顶点的序号<=k
+    for (int k = 0; k < node_number; k++)
+    {
+        for (int i = 0; i < node_number; i++)
+        {
+            for (int j = 0; j < node_number; j++)
+            {
+                if (a[i][k] < INF && a[k][j] < INF && (a[i][j] == INF || a[i][k] + a[k][j] < a[i][j]))
+                {
+                    // i = k && j != k，故a[i][j]的改变不影响本次k中其他i，j的计算，因为其若改变，是根据a[i][k]和a[k][j]计算的
+                    a[i][j] = a[i][k] + a[k][j];
+                    // i到j路经k(i->...->k->...->j)，从而i到j的路上j前面的顶点就是k到j的路上j前面的顶点
+                    path[i][j] = path[k][j];
+                }
+            }
+        }
+    }
+}
+
+// 假设各结点标号顺序即为拓扑顺序
+void Graph::critical_path(int* cp)
+{
+    int index = 0;
+    int *ve = new int[node_number], *vl = new int[node_number];
+    for (int i = 0; i < node_number; i++)
+    {
+        ve[i] = 0;
+    }
+    
+    // 计算ve: 每个点可能的最早开始时间; ve[j] = max{ve[i]+dur(i,j)}, i遍历所有指向j的结点
+    for (int i = 1; i < node_number; i++)
+    {
+        edge* p = targets[i].neighbour;
+        while (p)
+        {
+            if (ve[i] < ve[p->node] + p->cost)
+            {
+                ve[i] = ve[p->node] + p->cost;
+            }
+            p = p->next;
+        }
+    }
+    
+    // 计算vl: 每个点最迟开始时间; vl[j] = min{vl[k]-dur(j,k)}, k遍历所有j指向的结点
+    for (int i = 0; i < node_number; i++)
+    {
+        vl[i] = ve[node_number-1];
+    }
+    for (int i = node_number-2; i >= 0; i--)
+    {
+        edge* p = sources[i].neighbour;
+        while (p)
+        {
+            if (vl[i] > vl[p->node] - p->cost)
+            {
+                vl[i] = vl[p->node] - p->cost;
+            }
+            p = p->next;
+        }
+    }
+    
+    for (int i = 0; i < node_number; i++)
+    {
+        edge* p = sources[i].neighbour;
+        while (p)
+        {
+            if (ve[i] == vl[p->node] - p->cost)
+            {
+                cp[index++] = i;
+                cp[index++] = p->node;
+            }
+            p = p->next;
+        }
+        
+    }
+    
+    delete[] ve;
+    delete[] vl;
+}
+
+bool Graph::topological_sort(int seq[])
+{
+    if (undirected)
+    {
+        return false;
+    }
+    
+    int* count = new int[node_number];
+    int top = -1, index = 0;
+    // 统计入度并初始化栈，所有入度为0的入栈
+    // 其中栈为链栈，每个栈元素count值为其在栈中下方元素的下标，top为栈顶元素下标
+    get_indegree(count, top);
+    while (top != -1)   // 栈不空
+    {
+        // 出栈
+        seq[index] = top;
+        top = count[top];
+
+        edge* p = sources[seq[index]].neighbour;
+        // 删去以栈顶结点为源点的所有边
+        while (p)
+        {
+            if ((--count[p->node]) == 0)    // 入度变为0则进栈
+            {
+                count[p->node] = top;
+                top = p->node;
+            }
+            p = p->next;
+        }
+        
+        ++index;
+    }
+    
+    delete[] count;
+
+    // 有环
+    if (index < node_number)
+    {
+        return false;
+    }
+    return true;
 }
